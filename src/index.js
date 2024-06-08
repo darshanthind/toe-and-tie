62fcd04fc9b3f6842f5afef4b50676e19ef300aa
Gurpreet Singh <gurpreetsinghthind1001@gmail.com>
2024-06-08 12:48:01 +0530

    link delete index

--- src/index.js
+++ src/index.js
@@ -3,21 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Gallery from './workpages/practice1';
-import Login from './workpages/login_page';
-import Com from './pratice';
-import Counter from './pratice';
-import Formvalidation from './practice';
-import Home from './workpages/home_page';
-import Headeres from './workpages/header';
-import Apps from './workpages/validtion';
-import Sigup from './workpages/form';
-import P from './workpages/p';
-import Simple from './workpages/simple';
-import HomeBlog from './blo_nav';
-import HomeGame from './game_page/start';
-import Booking from './booking/Booking';
-import { Componentone } from './game_page/k';
+
+import { Componentone } from './game_page/toe_and_tie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
